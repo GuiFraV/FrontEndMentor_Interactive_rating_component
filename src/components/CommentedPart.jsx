fc8c62ece908771b430b8b75cfb5b79e9fc9984e
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-04-23 15:11:43 +0200

    Update CommentedPart component to include hover effect and thank you message

--- src/components/CommentedPart.jsx
+++ src/components/CommentedPart.jsx
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import paiement from "/payment_Two Color.png";
 
 
 const CommentedPart = ({ selectedRating , handleBackToUncommented}) => {
+  
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true)
+  }
+
+  const handleMouseLeave = () => {
+    setIsHovered(false)
+  }
+  
+  
+  
+  
   return (
     <div className="h-full w-full flex flex-col justify-evenly items-center">
       <img src={paiement} alt="paiement" width={162} height={108} className="" />
-      <p className="text-orange flex justify-center align-center w-[193px] h-[32px] rounded-[22.5px] bg-darkblue">
+      <p className="text-orange flex justify-center items-center w-[193px] h-[32px] rounded-[22.5px] bg-darkblue">
         You selected {selectedRating} out of 5
       </p>
   
       <button 
-        className="hover:bg-orange duration-500 py-2 px-4 rounded-[22.5px] h-[45px] w-[341px]"
-        onClick={handleBackToUncommented}  
+        className="hover:bg-orange duration-500 py-2 px-4 rounded-full h-[45px] w-[341px]"
+        onClick={handleBackToUncommented}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}  
       >
-        <h1 className=" text-white text-3xl font-bold">Thank You !</h1>
+        <h1 className=" text-white text-3xl font-bold">
+          {isHovered ? "Go Back !" : " Thank You !"}
+        </h1>
       </button>
 
       <p className="text-lightGrey text-center w-[365px]">
