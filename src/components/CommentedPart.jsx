2fa0700000ec414bfaa6528fa9fb30f378d5b681
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-04-23 14:48:20 +0200

    Update CommentedPart component to include a thank you message

--- src/components/CommentedPart.jsx
+++ src/components/CommentedPart.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import paiement from "/payment_Two Color.png";
-import thank from "/Thank.png";
+
 
 const CommentedPart = ({ selectedRating }) => {
   return (
@@ -9,7 +9,9 @@ const CommentedPart = ({ selectedRating }) => {
       <p className="text-orange flex justify-center align-center w-[193px] h-[32px] rounded-[22.5px] bg-darkblue">
         You selected {selectedRating} out of 5
       </p>
-      <img src={thank} alt="paiement" width={162} height={108} className="" />
+  
+      <h1 className=" text-white text-3xl font-bold">Thank You !</h1>
+
       <p className="text-lightGrey text-center w-[365px]">
         We appreciate you taking the time to give a rating. If you ever need
         more support, don't hesitate to get in touch!
